41ae38795f00ea37087a02004014fa2341d182c6
katsuya-ishiyama <katsuya.ishiyama@enish.com>
2018-08-03 02:23:02 +0900

    inplemented load_data

--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,39 @@
 # -*- coding: utf-8 -*-
 
 import pathlib
+from keras.utils import np_utils
 from skimage.io import imread
 
-image_dir = pathlib.Path('/home/ishiyama/notebooks/keyakizaka/mobilenet/')
 
-member_id_str = '001'
-member_dir = image_dir / member_id_str
-member_id = int(member_id_str)
+def load_data(image_dir: str, one_hot=True):
+    """ Load dataset of keyakizaka
+
+    Arguments
+    ---------
+    image_dir: str
+        A path of the directory that images are saved in.
+
+    one_hot: bool
+        if True then the labels will be convert to one hot
+        representation, default is True.
+
+    Returns:
+        a tuple of images and labels
+    """
+    X = []
+    Y = []
+
+    for member_dir in pathlib.Path(image_dir).iterdir():
+        member_id = int(member_dir.name)
+
+        for i, image_path in enumerate(member_dir.iterdir()):
+            X.append(imread(image_path))
+            Y.append(member_id)
+            if i >= 2:
+                break
+
+    if one_hot:
+        Y = np_utils.to_categorical(Y)
+
+    return (X, Y)
 
